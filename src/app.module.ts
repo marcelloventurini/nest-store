d345781ec38e7a5d5ecebd3ea124294d77c96859
Marcello Venturini <marcellofventurini@gmail.com>
2026-04-30 16:02:56 -0300

    chore: finish typeorm settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module.js';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service.js';
 import { ProductModule } from './product/product.module.js';
+import { UserModule } from './user/user.module.js';
 
-@Module({ imports: [UserModule, ProductModule] })
+@Module({
+  imports: [
+    UserModule,
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
+})
 export class AppModule {}
