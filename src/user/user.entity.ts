c88331e1439108be2c9680bd6d02b0ea8b7b2c09
Marcello Venturini <marcellofventurini@gmail.com>
2026-05-04 14:07:45 -0300

    feat: add typeorm decorators and audit timestamps to users

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,8 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id!: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
   name!: string;
+
+  @Column({ name: 'email', length: 70, unique: true, nullable: false })
   email!: string;
+
+  @Column({ name: 'password', length: 255, nullable: false })
   password!: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt!: string;
 
   constructor(partial: Partial<User>) {
     Object.assign(this, partial);
