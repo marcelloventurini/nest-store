76fca56893b1c90e32643b17fc723caaa0148c41
Marcello Venturini <marcellofventurini@gmail.com>
2026-05-13 14:14:51 -0300

    refactor: add extra fields to ListProductsDto

--- src/product/dto/list-product.dto.ts
+++ src/product/dto/list-product.dto.ts
@@ -2,5 +2,9 @@ export class ListProductDto {
   constructor(
     readonly id: string,
     readonly name: string,
+    readonly price: number,
+    readonly availableItems: number,
+    readonly description: string,
+    readonly category: string,
   ) {}
 }
